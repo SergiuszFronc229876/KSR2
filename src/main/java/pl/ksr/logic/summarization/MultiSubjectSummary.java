52340275bea5218814a4b1df4054f6d79d27a79c
PatrykGhot <patryk1259512595@gmail.com>
2023-06-05 04:08:12 +0200

    Revert "new type"
    This reverts commit 1562e41a0f579bf0a1d2016d5734817ff2b18af6.

--- src/main/java/pl/ksr/logic/summarization/MultiSubjectSummary.java
+++ src/main/java/pl/ksr/logic/summarization/MultiSubjectSummary.java
@@ -45,9 +45,5 @@ public interface MultiSubjectSummary {
         return result;
     }
 
-    default String subjectName(boolean isNew) {
-        return isNew ? "wyprodukowanych mniej niż 5 lat temu" : "wyprodukowanych ponad 5 lata temu";
-    }
-
     String toString();
 }
